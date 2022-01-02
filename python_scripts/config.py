db8b98f072407044650958ee7cd7d768fb5fd324
m-naim <mehenaouin@gmail.com>
2022-01-02 22:52:57 +0100

    delete py

--- python_scripts/config.py
+++
@@ -1,7 +0,0 @@
-from dotenv.main import dotenv_values
-from pymongo import MongoClient, ssl_support
-
-config = dotenv_values("../.env")
-client = MongoClient(config['DATABASE_URL'], ssl_cert_reqs=ssl_support.CERT_NONE)
-db=client['investing']
-# db= client
--- scheduled/updateStocks.js
+++ scheduled/updateStocks.js
@@ -5,17 +5,17 @@ const job = cron.schedule('0 * * * *', () => {
   console.log('running a task every hour');
   let dataToSend;
   // spawn new child process to call the python script
-  const python = spawn('python', ['python_scripts/update_last.py']);
+  // const python = spawn('python', ['python_scripts/update_last.py']);
   // collect data from script
-  python.stdout.on('data', (data) => {
-    console.log('Pipe data from python script ...');
-    dataToSend = data.toString();
-  });
+  // python.stdout.on('data', (data) => {
+  //   console.log('Pipe data from python script ...');
+  //   dataToSend = data.toString();
+  // });
   // in close event we are sure that stream from child process is closed
-  python.on('close', (code) => {
-    console.log(`child process close all stdio with code ${code}`);
-    // send data to browser
-    console.log(dataToSend);
-  });
+  // python.on('close', (code) => {
+  //   console.log(`child process close all stdio with code ${code}`);
+  //   // send data to browser
+  //   console.log(dataToSend);
+  // });
 });
 module.exports = job;
