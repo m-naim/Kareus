ead4c92ca932fbedcdc17db4bdb5e9f7845e7366
m-naim <mehenaouin@gmail.com>
2022-11-26 14:51:03 +0100

    v 2

--- client.v2/src/components/Table.js
+++ client.v2/src/components/Table.js
@@ -23,16 +23,16 @@ function Table({ propRows = data.rows, columns = data.columns, addtransaction, t
         toggle();
     }
 
-    const sortby= (column)=>{
-        setSortSens(sortSens*-1)
+    const sortby = (column) => {
+        setSortSens(sortSens * -1)
         setColumnSorting(column)
         console.log(column);
-        setRows(rows.sort((a,b)=>comparator(a[column],b[column])*sortSens))
+        setRows(rows.sort((a, b) => comparator(a[column], b[column]) * sortSens))
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         setRows(propRows)
-    },[propRows])
+    }, [propRows])
 
     return (
         <div >
@@ -46,54 +46,57 @@ function Table({ propRows = data.rows, columns = data.columns, addtransaction, t
                     <AddTransaction hide={toggle} addClick={addtransaction} symbol={ticker} />
                 </Modal>
             </div>
-            <table class="table-fixed w-full">
+            <table class="w-full divide-y border-gray-200 ">
                 <thead>
-                    <tr className="border-collapse border-b border-gray-200 ">
-                        <th></th>
-                        {columns.map(c => 
-                        <th onClick={()=>sortby(c)} 
-                        class="text-left text-gray-600 dark:text-gray-300" >{c} 
-                        {c==columnSorting && ' S'}
-                        </th>)}
+                    <tr className="">
+                        {editable && <th></th>}
+                        {columns.map(c =>
+                            <th onClick={() => sortby(c)}
+                                class="text-left text-lg text-gray-500 dark:text-gray-300 py-2 font-light" >
+                                <div className='flex gap-1'>
+                                    {c}
+                                    {c == columnSorting && <img className={`${sortSens > 0 && 'rotate-180'} transition delay-75 duration-300 h-6`} src="https://img.icons8.com/material/24/228BE6/up--v1.png" />}
+                                </div>
+                            </th>)}
                     </tr>
                 </thead>
                 <tbody>
-                    {rows.map(r => type === 'allocation' ? AllocationLine(r, addclick,editable) : TransactionLine(r, addclick,editable))}
+                    {rows.map(r => type === 'allocation' ? AllocationLine(r, addclick, editable) : TransactionLine(r, addclick, editable))}
                 </tbody>
             </table>
         </div>
     );
 }
 
-const AllocationLine = (r, addclick,editable) => {
-    return (<tr className="border-collapse border-b border-gray-200 dark:border-slate-600">
-        <td className=''>
-            {editable &&
-            <button type="button"
-                onClick={() => addclick(r.symbol)}
-                class="bg-dark bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                <span class="sr-only">Add transaction</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16"> <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" /> </svg>
-            </button>
-            }
-        </td>
-        <td className=''>
+const AllocationLine = (r, addclick, editable) => {
+    return (<tr className="border-separate border-spacing-4 border-b border-gray-200 dark:border-slate-600 text-md font-medium ">
+        {editable &&
+            <td className='py-2'>
+                <button type="button"
+                    onClick={() => addclick(r.symbol)}
+                    class="bg-dark bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span class="sr-only">Add transaction</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16"> <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" /> </svg>
+                </button>
+            </td>
+        }
+        <td className='py-2'>
             <div className='flex flex-col w-fit '>
                 <p className=''>{r.asset.name}</p>
                 <p className='text-xs align-middle text-blue-500 rounded p-1 w-fit '>{r.symbol}</p>
             </div>
         </td>
-        <td className=''>
-            <p className='font-semibold'>{toPrecentille(r.weight)}</p>
+        <td className='py-2'>
+            <p className='text-primary'>{toPrecentille(r.weight)}</p>
         </td>
-        <td className=''>
+        <td className='py-2'>
             <p>{r.qty}</p>
         </td>
-        <td className=''>
-            <p>{round10(r.asset.last, -2)}</p>
+        <td className='py-2'>
+            <p>{round10(r.asset.last, -2)}€</p>
         </td>
-        <td className=''>
-            <p>{round10(r.bep, -2)}</p>
+        <td className='py-2'>
+            <p>{round10(r.bep, -2)}€</p>
         </td>
     </tr>);
 }
@@ -101,16 +104,16 @@ const AllocationLine = (r, addclick,editable) => {
 const TransactionLine = (r, addclick, editable) => {
     return (
         <tr className="border-collapse border-b border-gray-200 dark:border-slate-600">
-            <td >
-                {editable &&
+            {editable &&
+                <td >
                     <button type="button"
                         onClick={() => addclick(r.symbol)}
                         class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                         <span class="sr-only">Add transaction</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16"> <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" /> </svg>
                     </button>
-                }
-            </td>
+                </td>
+            }
             <td >
                 <div className='flex flex-col'>
                     <p>{r.symbol}</p>
